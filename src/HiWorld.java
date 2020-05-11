e1597e773cd4390f5e94808e0d6d3ad0e14e4833
tinydawn <2112166464@qq.com>
2020-05-11 15:29:48 +0800

    test

---
+++ src/HiWorld.java
@@ -0,0 +1,9 @@
+/**
+ * @author tinydawn
+ * @version 2020/5/11 15:27 V1.0
+ **/
+public class HiWorld {
+    public static void main(String[] args) {
+        System.out.println("hello,word");
+    }
+}
